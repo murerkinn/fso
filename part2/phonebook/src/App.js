7f2189791beefac1d3640c8a5d34fff0266dd8f8
Murat Erkin Çiçek <murerkinn@gmail.com>
2020-09-15 14:10:42 +0300

    Exercises 2.15-2.18 completed.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from "react";
 import Persons from "./Persons";
 import PersonForm from "./PersonForm"
 import Filter from "./Filter"
-import axios from "axios"
+import contr from "./controller"
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
-  const [results, setResults] = useState(persons)
-  const copy = [...persons];
+  const [filterVal, setFilterVal] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -21,33 +19,63 @@ const App = () => {
   };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value)
+    setFilterVal(event.target.value)
   }
 
-  const addPerson = (event) => {
-    event.preventDefault();
+  const updatePersonNum = e => {
+    e.preventDefault()
+  }
 
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      copy.push({ name: newName, number: newNumber });
-      setPersons(copy);
+  const addPerson = e => {
+    e.preventDefault()
+    if (newName === "" || !/^[a-z öçişğü]*$/i.test(newName)) {
+      alert('Please enter a valid name')
     }
-  };
 
-  useEffect(() => {
-    axios.get("http://localhost:3001/persons")
-    .then((res) => {
+    if (newNumber === "" || /[^\d -]/.test(newNumber)) {
+      alert('Please enter a valid phone number')
+    }
+
+    if (persons.some(item => item.name === newName)) {
+      if (newNumber === "" || /[^\d -]/.test(newNumber)) {
+        alert('Please enter a valid phone number')
+      }
+      alert(`${newName} already exists in the phonebook.`)
+    }
+    setPersons([
+      ...persons,
+      {
+        name: newName,
+        number: newNumber.replace(/-+|\s+/g, "-").replace(/^-*(.+?)-*$/, "$1")
+      }
+    ])
+    contr.create({name: newName, number: newNumber})
+    setNewName("")
+    setNewNumber("")
+  }
+
+  const filterPersons = () => {
+    if(filterVal === "") {
+      return persons
+    }
+    return [...persons].filter(
+      item => item.name.toLowerCase().indexOf(filterVal.toLowerCase()) !== -1
+    )
+  }
+
+  const hook = () => {
+    contr.getAll()
+    .then(res => {
       setPersons(res.data)
     })
-    const end = persons.filter(person => person.name.toLowerCase().includes(filter))
-    setResults(end)
-  }, [filter, persons])
+  }
+
+  useEffect(hook, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Filter filter={filterVal} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm
         addPerson={addPerson}
@@ -57,7 +85,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={results} />
+      <Persons persons={filterPersons()} />
     </div>
   );
 };
--- part2/phonebook/src/Persons.js
+++ part2/phonebook/src/Persons.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import contr from './controller'
 
 function Persons(props) {
+    function del(id){
+        if(window.confirm(`Are you sure that you want to remove this information ?`)) {
+            contr.remove(id)
+            .then(window.location.reload())
+        }
+    }
     return (
         <div>
             <ul>
                 {props.persons.map(person => 
-                <p key={person.name} ><strong>Name:</strong> {person.name} ~ <strong>Number:</strong> {person.number}</p>
+                    <p key={person.name} ><strong>Name:</strong> {person.name} ~ <strong>Number:</strong> {person.number} <button onClick={() => del(person.id)}>delete</button></p>
                 )}
             </ul>
         </div>
